47eace5c8e617f479ea9391e3bbdebf543d8178a
JMr <jramallo@ombu.net>
2022-03-07 18:06:52 -0300

    Carga de Avances

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const ejes = require('ejs');
+
+app.set('view engine','ejs');
+
+app.get('/', (req, res) => {
+    res.send('Hola mundo');
+});
+
+//Static
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => console.log('Listening on port 3000'));
+
