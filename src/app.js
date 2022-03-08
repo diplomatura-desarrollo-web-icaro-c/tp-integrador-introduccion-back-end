7a3254de8074881e41f99612af769b4c2688fa4b
Norberto <norberto.cabrera.a@gmail.com>
2022-03-07 22:07:17 -0300

    muestra por pantalla de datos de persona usando ejs en bienvenidos.js

--- src/app.js
+++ src/app.js
@@ -5,12 +5,25 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+/* --------------------------------------------- */
+
+const persona = {
+  nombre: "norberto",
+  apellido: "cabrera",
+  estadoCivil: "soltero",
+  enServicio: true,
+  hobbies: ["futbol", "natacion", "correr"],
+};
+/* --------------------------------------------- */
+
 app.get("/", (req, res) => {
-  res.send("uri para todas las rutas!");
+  res.render("home");
 });
 
+app.get("/bienvenidos", (req, res) => {
+  res.render("bienvenidos", { persona: persona });
+});
 app.use(express.static(path.join(__dirname, "../public/")));
-
 app.listen(3000, () => console.log("listening on port 3000"));
 
 /* en el caso de login me deja acceder a su contenido desde el navegador unicamente si
