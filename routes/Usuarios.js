aaadb1b6b9f0ab5ba1fd7d29d308c42c3d301417
Norberto <norberto.cabrera.a@gmail.com>
2022-03-10 15:02:54 -0300

    comienzo desde cero

--- routes/Usuarios.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const express = require("express");
-
-const router = express.Router();
-const GetUser = require("../controlador/Usuarios");
-
-router.get("/:id", GetUser);
-
-module.exports = router;
