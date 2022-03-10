5dc3d07da7556f5c033cb86a8a1aef665b2cb3ae
Norberto <norberto.cabrera.a@gmail.com>
2022-03-10 15:31:27 -0300

    creacion del archivo DatosDeUsuario.js en carpeta modelo

---
+++ modelo/DatosDeUsuarios.js
@@ -0,0 +1,24 @@
+/* aqui en modelo simularemos la obtencion de datos desde una base de datos */
+
+let USERS_INFORMATION = [
+  {
+    id: 1,
+    nombre: "norberto",
+    apellido: "cabrera",
+    email: "norberto.cabrera.a@gmail.com",
+  },
+  {
+    id: 2,
+    nombre: "julian",
+    apellido: "perez",
+    email: "julian.perez@gmail.com",
+  },
+  {
+    id: 3,
+    nombre: "roberto",
+    apellido: "alvarez",
+    email: "roberto.alvarez@gmail.com",
+  },
+];
+
+module.exports = { USERS_INFORMATION: USERS_INFORMATION };
