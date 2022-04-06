ca94c436258aa2bc5664f60656a324f95ab30f92
Juan José Barberis <juan.jose.barberis@mi.unc.edu.ar>
2022-04-06 16:44:57 -0300

    crear sesion con express-session

--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
 // Make express server
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
 
-//Declaro carpeta estatica public para guardar css
-const path = require('path')
+//express validator
+/* const { body, validationResult } = require('express-validator'); */
 
-app.use(express.static(path.join(__dirname, '../public')),
-  bodyParser.urlencoded({ extended: true })
-);
+//BODY-PARSER
+const bodyParser = require('body-parser');
 
+//Requerimos rutas
+const userRoutes = require("../routes/Usuarios");
+
+/* yo */
+//session y cookies
+const sessions = require('express-session');
+const unDia = 1000*60*60*24;
+const unMinuto = 1000*60;
+//const cookieParser=require('cookie-parser');
+
+//usamos sesiones
+app.use(sessions({
+	secret: "123456",
+	resave: false,
+	saveUninitialized:true, //true ya depreciado config a false ver doc
+	cookie: { maxAge:unDia }
+    //cookie: { maxAge:unMinuto }
+}));
+
+//app.use(cookieParser())
+/* fin yo */
+
+//usamos body-parser
+app.use(bodyParser.urlencoded({extended:true}))
+
+//declaro carpeta estatica public para guardar css
+const path = require('path');
+app.use(express.static(path.join(__dirname,'../public')));
+
+//seteamos visor de plantilla ejs
 app.set('view engine', 'ejs')
 
-const userRoutes = require("../routes/Usuarios");
 app.use("/usuarios", userRoutes);
 
 app.listen(PORT, () => {
-  console.log("Servidor corriendo en puerto ", PORT);
+  console.log("Servidor corriendo en puerto ",PORT);
 });
