780a12efc8f429109a442a7ab45e359829404d98
Juan José Barberis <juan.jose.barberis@mi.unc.edu.ar>
2022-04-11 20:23:18 -0300

    fix header

--- controladores/Usuarios.js
+++ controladores/Usuarios.js
@@ -22,7 +22,9 @@ const GetFormulario = (req, res) => {
   res.render('vistas/formulario');
 };
 
-const ProcesarFormulario = (req, res) => {
+//asi quedo ultima clase pero con forEach, y error de header.
+
+/* const ProcesarFormulario = (req, res) => {
   var nombre = req.body.nombre;
   var apellido = req.body.apellido;
 
@@ -45,10 +47,65 @@ const ProcesarFormulario = (req, res) => {
 
   res.send("<h1> Usuario incorrecto volver al <a href='/usuarios'> formulario</a> </h1>")
 
+} */
 
+
+// Arreglado problema de header
+
+const ProcesarFormulario = (req, res) => {
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  let userFound=false
+  const userList = QueryListOfUsers();
+
+  userList.forEach(usuario => {
+    if(usuario.nombre == nombre && usuario.apellido == apellido){
+      userFound=true;
+    }
+  });
+
+  if(userFound==true){
+    var usuarioObtenido = {
+        nombre,
+        apellido
+      }
+    
+      res.render('vistas/vistaUsuario', {
+        usuario: usuarioObtenido
+      })
+      
+
+  } else{ 
+    res.send("<h1> Usuario incorrecto volver al <a href='/usuarios'> formulario</a> </h1>")
+    
+  }
 }
 
+// con for pero con error de header
+/* const ProcesarFormulario = (req, res) => {
+  var vectorUsuarios = QueryListOfUsers();
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
 
+  for (let i = 0; i < vectorUsuarios.length; i++) {
+    if (vectorUsuarios[i].nombre.toLowerCase() == nombre.toLowerCase()
+      && vectorUsuarios[i].apellido.toLowerCase()  == apellido.toLowerCase()) {
+      var usuarioObtenido = {
+        id:vectorUsuarios[i].id,
+        nombre:vectorUsuarios[i].nombre,
+        apellido:vectorUsuarios[i].apellido,
+        email:vectorUsuarios[i].email
+      }
+
+      res.render('vistas/vistaUsuario', {
+        usuario: usuarioObtenido
+      })
+    }
+  }
+  console.log('esto sigue por aqui')
+  res.send("<h1> Usuario incorrecto volver al <a href='/usuarios'> formulario</a> </h1>")
+
+} */
 
 module.exports = {
   GetAllUsers,
