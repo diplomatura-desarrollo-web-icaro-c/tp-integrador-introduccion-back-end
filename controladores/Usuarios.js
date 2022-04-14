a96f0e27b9d90e2e90224042037e3f179aef18f8
Juan José Barberis <juan.jose.barberis@mi.unc.edu.ar>
2022-04-14 00:14:52 -0300

    crear api procesar formulario

--- controladores/Usuarios.js
+++ controladores/Usuarios.js
@@ -15,16 +15,10 @@ const GetAllUsers = (req, res) => {
   return respuesta;
 };
 
-/* const GetAllUsers = (req, res) => {
-  const userList = QueryListOfUsers();
-  return res.json(userList);
-}; */
-
 
 // con API
 const GetUsuario = (req, res) => {
   const userId = req.params.id;
-  /* const usuarioObtenido = QueryUserById(userId); */
   let respuesta = fetch('http://localhost:3001/'+userId)
   .then(response => response.json())
   .then(data => res.render('vistas/vistaUsuario', {
@@ -36,106 +30,36 @@ return respuesta
 };
 
 
-//antes de api
-/* const GetUsuario = (req, res) => {
-  const userId = req.params.id;
-  const usuarioObtenido = QueryUserById(userId);
-
-  res.render('vistas/vistaUsuario', {
-    usuario: usuarioObtenido
-  })
-}; */
-
-
-
 const GetFormulario = (req, res) => {
   res.render('vistas/formulario');
 };
 
-//asi quedo ultima clase pero con forEach, y error de header.
-
-/* const ProcesarFormulario = (req, res) => {
-  var nombre = req.body.nombre;
-  var apellido = req.body.apellido;
-
-  const userList = QueryListOfUsers();
-
-  userList.forEach(usuario => {
-    if(usuario.nombre == nombre && usuario.apellido == apellido){
-
-      var usuarioObtenido = {
-        nombre,
-        apellido
-      }
-    
-      res.render('vistas/vistaUsuario', {
-        usuario: usuarioObtenido
-      })
-    }
-
-  });
-
-  res.send("<h1> Usuario incorrecto volver al <a href='/usuarios'> formulario</a> </h1>")
-
-} */
-
-
-// Arreglado problema de header
 
 const ProcesarFormulario = (req, res) => {
   var nombre = req.body.nombre;
   var apellido = req.body.apellido;
-  let userFound=false
-  const userList = QueryListOfUsers();
+  console.log('http://localhost:3001/usuarios?nombre='+nombre+'&apellido='+apellido);
+  fetch('http://localhost:3001/usuarios?nombre='+nombre+'&apellido='+apellido)
+     .then(response => response.json())
+     .then(data => {
 
-  userList.forEach(usuario => {
-    if(usuario.nombre == nombre && usuario.apellido == apellido){
-      userFound=true;
-    }
-  });
-
-  if(userFound==true){
-    var usuarioObtenido = {
-        nombre,
-        apellido
-      }
-    
-      res.render('vistas/vistaUsuario', {
-        usuario: usuarioObtenido
-      })
+      console.log(data);
       
-
-  } else{ 
-    res.send("<h1> Usuario incorrecto volver al <a href='/usuarios'> formulario</a> </h1>")
-    
-  }
+      if(data==true){
+        var usuarioObtenido = {
+            nombre,
+            apellido
+          }
+        
+          res.render('vistas/vistaUsuario', {
+            usuario: usuarioObtenido
+          })
+      } else{ 
+        res.send("<h1> Usuario incorrecto volver al <a href='/usuarios'> formulario</a> </h1>")
+      }}
+      )
 }
 
-// con for pero con error de header
-/* const ProcesarFormulario = (req, res) => {
-  var vectorUsuarios = QueryListOfUsers();
-  var nombre = req.body.nombre;
-  var apellido = req.body.apellido;
-
-  for (let i = 0; i < vectorUsuarios.length; i++) {
-    if (vectorUsuarios[i].nombre.toLowerCase() == nombre.toLowerCase()
-      && vectorUsuarios[i].apellido.toLowerCase()  == apellido.toLowerCase()) {
-      var usuarioObtenido = {
-        id:vectorUsuarios[i].id,
-        nombre:vectorUsuarios[i].nombre,
-        apellido:vectorUsuarios[i].apellido,
-        email:vectorUsuarios[i].email
-      }
-
-      res.render('vistas/vistaUsuario', {
-        usuario: usuarioObtenido
-      })
-    }
-  }
-  console.log('esto sigue por aqui')
-  res.send("<h1> Usuario incorrecto volver al <a href='/usuarios'> formulario</a> </h1>")
-
-} */
 
 module.exports = {
   GetAllUsers,
